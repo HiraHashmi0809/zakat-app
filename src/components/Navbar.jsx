c3c7ffed2e1a5e377fc3200c1c8f8493432118cd
ahmad-hashmi <ahmadhashmi123@gmail.com>
2025-09-19 18:59:54 +0500

    update: fixed logo path and vercel config

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -82,7 +82,14 @@ function Navbar() {
     <nav className="navbar">
       <div className="brand">
         {/* ✅ Public folder safe image for Vite */}
-        <img src="/logo-app.jpeg" alt="Al Khair Trust Logo" />
+        {/* <img src="/logo-app.jpeg" alt="Al Khair Trust Logo" />
+          */}
+          <img 
+  src="/logo-app.jpeg" 
+  alt="Al Khair Trust Logo" 
+  className="w-32 h-auto object-contain" 
+/>
+
         <div className="logo">Al Khair Trust</div>
       </div>
 
