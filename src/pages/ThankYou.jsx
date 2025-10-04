c34a03574504b9f26c066d1f0f7607b2911ecaf9
ahmad-hashmi <ahmadhashmi123@gmail.com>
2025-10-04 13:37:51 +0500

    Fix: correct import path for thankyou.css

--- src/pages/ThankYou.jsx
+++ src/pages/ThankYou.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "../styles/thankyou.css";
 
+
+
 function ThankYou() {
   return (
     <div className="thankyou-container">
